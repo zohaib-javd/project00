3b18aefb00d488cced5806d215b20231f2050102
Zohaib Javed <zohaibjaved@gmail.com>
2024-02-14 13:49:47 +0500

    first project files

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log("Hello, World");
+console.log("My Name is Zohaib Javed");
+console.log("This is my first assignment of typescript");
+console.log("TypeScript extends JavaScript by adding types.");
+console.log("It's a beautiful day to learn something new!");
+console.log("I'm excited to dive deeper into TypeScript.");
